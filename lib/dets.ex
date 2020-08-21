906e7540f7d1a68a89dfb9ec8e00ab0ab4739b38
Adam Richard <arichard44@gmail.com>
2020-08-20 23:53:40 -0400

    dets keys...

--- lib/dets.ex
+++ lib/dets.ex
@@ -1,16 +1,19 @@
 defmodule Wargear.Dets do
 
   def insert(table, key, val) do
+    atom_key = :"#{to_string(key)}"
     :dets.open_file(table, [{:file, "#{table}.txt" |> String.to_charlist() }])
-    :dets.insert(table, {key, val})
+    :dets.insert(table, {atom_key, val})
     :dets.close(table)
   end
 
   def lookup(table, key, default) do
     :dets.open_file(table, [{:file, "#{table}.txt" |> String.to_charlist()}])
 
+    atom_key = :"#{to_string(key)}"
+
     val = 
-      case :dets.lookup(table, key) do
+      case :dets.lookup(table, atom_key) do
         [{^key, val}] -> val
         _ -> default
       end
