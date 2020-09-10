9b891ecffa31604c82bb600f07dc77d3a0469603
Adam Richard <arichard44@gmail.com>
2020-09-10 19:22:25 -0400

    handle endpoint new game

--- lib/resolver/resolver_game.ex
+++ lib/resolver/resolver_game.ex
@@ -1,22 +1,10 @@
 defmodule Wargear.Resolver.Game do
-
-  # defmodule GameDao do
-  #   alias Wargear.Dets
-  #   @table :game_info
-  #   @key :game_id
-  #   @initial_state nil
-  #   def update(game_id), do: Dets.insert(@table, @key, game_id)
-  #   def get, do: Dets.lookup(@table, @key, @initial_state)
-  #   def reset, do: Dets.insert(@table, @key, @initial_state)
-  # end
   
-  def new(%{game_id: _game_id}, _info) do
-    # poller_spec = { Wargear.Events.Poller, [game_id: 743165] }
-    # handler_spec = { Wargear.Events.Handler, [game_id: 743165] }
-    # {:ok, poller} = DynamicSupervisor.start_child(GameSupervisor, poller_spec)
-    # # Dets insert Handlergame_id -> pid
-    # {:ok, handler} = DynamicSupervisor.start_child(GameSupervisor, handler_spec)
-    # # Dets insert game_id -> [poller, handler]
+  def new(%{game_id: game_id}, _info) do
+    poller_spec = { Wargear.Events.Poller, [game_id: game_id] }
+    handler_spec = { Wargear.Events.Handler, [game_id: game_id] }
+    {:ok, _poller} = DynamicSupervisor.start_child(GameSupervisor, poller_spec)
+    {:ok, _handler} = DynamicSupervisor.start_child(GameSupervisor, handler_spec)
     {:ok, true}
   end
 
